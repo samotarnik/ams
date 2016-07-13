615d879f593af9432eba8cfaa2aabf6d3b9aa620
samo ratnik <samo.ratnik@gmail.com>
2016-07-13 22:28:18 +0200

    refactor specs

--- spec/ams/models/artist_spec.rb
+++ spec/ams/models/artist_spec.rb
@@ -3,32 +3,30 @@ require 'ams/models/artist'
 describe Ams::Models::Artist do
 
   describe 'attributes:' do
-    before(:each) do
-      @artist = described_class.new
-    end
+    let(:artist) { described_class.new }
 
     it 'has a name' do
-      @artist.name = 'kekec'
-      expect(@artist.name).to eq 'kekec'
+      artist.name = 'kekec'
+      expect(artist.name).to eq 'kekec'
     end
 
     it 'has an allmusic id' do
-      @artist.am_id = 'mn09532453'
-      expect(@artist.am_id).to eq 'mn09532453'
+      artist.am_id = 'mn09532453'
+      expect(artist.am_id).to eq 'mn09532453'
     end
 
     it 'has a url part that also sets the allmusic id' do
-      @artist.url_part = 'radiohead-mn95435243'
-      expect(@artist.url_part).to eq 'radiohead-mn95435243'
-      expect(@artist.am_id).to eq 'mn95435243'
+      artist.url_part = 'radiohead-mn95435243'
+      expect(artist.url_part).to eq 'radiohead-mn95435243'
+      expect(artist.am_id).to eq 'mn95435243'
 
-      @artist.url_part = nil
-      expect(@artist.url_part).to be nil
-      expect(@artist.am_id).to be nil
+      artist.url_part = nil
+      expect(artist.url_part).to be nil
+      expect(artist.am_id).to be nil
 
-      @artist.am_id = 43
-      expect(@artist.url_part).to be nil
-      expect(@artist.am_id).to eq 43
+      artist.am_id = 43
+      expect(artist.url_part).to be nil
+      expect(artist.am_id).to eq 43
     end
   end
 
